432849bb81593ddf6a8c19ab0eaa2325f093fefc
Liam Cooper <liamcoop1997@gmail.com>
2021-01-20 07:37:06 -0800

    cleaned up code formatting, banner styling.

--- client/src/components/TimeBanner/index.tsx
+++ client/src/components/TimeBanner/index.tsx
@@ -7,24 +7,21 @@ import {
 
 export function TimeBanner(time: ConditionsObj) {
     return (
-            <span id="banner">
+        <span id="banner">
+            <p>
+                {time.dt_txt.split(' ')[1].split(':')[0]}:
+                {time.dt_txt.split(' ')[1].split(':')[1]}
+            </p>
+            <Icon icID={time.weather[0].icon} imgHeight={"45vh"}/>
+            <div id="tempSnow">
                 <p>
-                    {time.dt_txt.split(' ')[1].split(':')[0]}:
-                    {time.dt_txt.split(' ')[1].split(':')[1]}
+                    { Math.round(
+                        (time.main.temp_min - time.main.temp_max) / 2 + 
+                            time.main.temp_max)
+                    } &deg;C
                 </p>
-                <Icon icID={time.weather[0].icon} imgHeight={"45vh"}/>
-                <div id="tempSnow">
-                    <p>
-                        { Math.round(
-                            (time.main.temp_min - time.main.temp_max) / 2 + 
-                                time.main.temp_max
-                        )
-                        }&deg;C</p>
-                    {
-                        time.snow ? 
-                            <p>{time.snow?.['3h']}cm</p> : <p>no new snow</p>
-                    }
-                </div>
-            </span>
+                {<p>{time.snow ? time.snow?.['3h'] : '0'}cm</p>}
+            </div>
+        </span>
     );
 }
