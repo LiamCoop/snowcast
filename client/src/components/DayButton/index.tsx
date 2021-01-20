7f2a5825cd7c6eb09c8cee77009fadf4cd7d7ff1
Liam Cooper <liamcoop1997@gmail.com>
2021-01-20 08:12:32 -0800

    added method to display snowfall for the given day.

--- client/src/components/DayButton/index.tsx
+++ client/src/components/DayButton/index.tsx
@@ -8,12 +8,17 @@ import {
 import './DayButton.css';
 
 export function DayButton(props: {day:DayObj} ) {
+    let daySnow = 0;
+    props.day.list.map((time) => {
+       daySnow += time.snow ? time.snow?.['3h'] : 0;
+    });
     return (
         <>
             <h1>{props.day.date}</h1>
             <div>
                 <Icon icID={props.day.list[0].weather[0].icon} imgHeight={"70vh"}/>
             </div>
+            <p>Snow: {Math.round(daySnow)}cm</p>
         </>
     );
 }
