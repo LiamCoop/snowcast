a9979e909ac5ca7421eb229a94a58dfd93c55290
Liam Cooper <liamcoop1997@gmail.com>
2021-01-20 08:13:06 -0800

    corrected wrong img src

--- client/src/components/icons/ClearSky.tsx
+++ client/src/components/icons/ClearSky.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function ClearSky(hgt:string) {
   return (
     <img
-      src="http://openweathermap.org/img/wn/00d@2x.png"
+      src="http://openweathermap.org/img/wn/01d@2x.png"
       alt="ClearSky"
       height={hgt}
     />
