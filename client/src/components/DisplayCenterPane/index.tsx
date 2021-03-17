32f79ce9ee265c540711b33ca59e351a1bbb7c38
Liam Cooper <liamcoop1997@gmail.com>
2021-03-17 06:45:06 -0700

    Moving along w/ using styled-components

--- client/src/components/DisplayCenterPane/index.tsx
+++ client/src/components/DisplayCenterPane/index.tsx
@@ -1,5 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { DayObj, Icon, TimeBanner } from "../";
+import {
+    H1,
+    P,
+    Area,
+    Card,
+    City,
+    Col,
+    Row,
+    CurrentPane,
+    Dt,
+    PropArea,
+    PropCity,
+    Temp,
+    TempText,
+    TempTextp,
+    Title,
+    TitleContainer,
+    Weather,
+    WeatherText,
+} from "./style";
 
 import "./DisplayCenterPane.css";
 
@@ -22,84 +42,80 @@ function DisplayCenterPane(props: {
 
     return (
         <>
-            <div className="titlecontainer">
-                <h1 className="city title">City:</h1>
-                <h1 className="propCity title">{props.city}</h1>
-                <h1 className="dt title">{currentTime.dt_txt.split(" ")[0]}</h1>
-                <h1 className="area title">Ski Area:</h1>
-                <h1 className="propArea title">{props.skiAreaName}</h1>
-            </div>
-            <div className="row">
-                <div className="currentPane">
-                    <div className="col">
-                        <h1>
+            <TitleContainer>
+                <Title>City:</Title>
+                <Title>{props.city}</Title>
+                <Title>{currentTime.dt_txt.split(" ")[0]}</Title>
+                <Title>Ski Area:</Title>
+                <Title>{props.skiAreaName}</Title>
+            </TitleContainer>
+            <Row>
+                <CurrentPane>
+                    <Col>
+                        <H1>
                             {currentTime.dt_txt.split(" ")[1].split(":")[0]}h
                             {currentTime.dt_txt.split(" ")[1].split(":")[1]}
-                        </h1>
+                        </H1>
                         <Icon
                             icID={currentTime.weather[0].icon}
                             imgHeight={"100vh"}
                         />
-                        <p>Conditions: {currentTime.weather[0].description}</p>
-                    </div>
-                    <div className="row">
-                        <div className="temp">
-                            <h1>
+                        <P>Conditions: {currentTime.weather[0].description}</P>
+                    </Col>
+                    <Row>
+                        <Temp>
+                            <H1>
                                 {Math.round(currentTime.main.temp)}&deg;
                                 {props.units === "metric" ? "C" : "F"}
-                            </h1>
-                            <p>
+                            </H1>
+                            <P>
                                 {Math.round(currentTime.main.temp_min)}&deg;
                                 {props.units === "metric" ? "C" : "F"} /{" "}
                                 {Math.round(currentTime.main.temp_max)}&deg;
                                 {props.units === "metric" ? "C" : "F"}
-                            </p>
-                        </div>
-                        <div className="weather">
-                            <p className="pop">
+                            </P>
+                        </Temp>
+                        <Weather>
+                            <P>
                                 Precipitation:{" "}
                                 {Math.round(currentTime.pop * 100)}%
-                            </p>
-                            <p className="hum">
+                            </P>
+                            <P>
                                 Humidity:{" "}
                                 {Math.round(currentTime.main.humidity)}%
-                            </p>
+                            </P>
                             {currentTime.visibility === 10000 ? (
-                                <p className="vis">Vis: &infin; m</p>
+                                <P>Vis: &infin; m</P>
                             ) : (
-                                <p className="vis">
-                                    Vis: {currentTime.visibility} m
-                                </p>
+                                <P>Vis: {currentTime.visibility} m</P>
                             )}
-                            <p className="cloudiness">
-                                Cloud: {currentTime.clouds.all}%
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <p>
+                            <P>Cloud: {currentTime.clouds.all}%</P>
+                        </Weather>
+                    </Row>
+                    <Col>
+                        <P>
                             Windspeed, direction:
                             {Math.round(currentTime.wind.speed)}
                             {props.units === "metric" ? "m/s" : "mph"},
                             {Math.round(currentTime.wind.speed)}&deg;
-                        </p>
+                        </P>
                         {currentTime.rain ? (
-                            <p>
+                            <P>
                                 Rainfall for previous 3hrs:{" "}
                                 {Math.round(currentTime.rain["3h"])} mm
-                            </p>
+                            </P>
                         ) : null}{" "}
                         {currentTime.snow ? (
-                            <p>
+                            <P>
                                 Snowfall over previous 3hrs: ~
                                 {Math.round(currentTime.snow["3h"] * 100) / 10}
                                 mm
-                            </p>
+                            </P>
                         ) : null}
-                    </div>
-                </div>
-                <div className="col">
-                    <p>3 Hour Intervals</p>
+                    </Col>
+                </CurrentPane>
+                <Col>
+                    <P>3 Hour Intervals</P>
                     {props.currentDay.list.map((timeSlice) => {
                         return (
                             <TimeBanner
@@ -111,8 +127,8 @@ function DisplayCenterPane(props: {
                             />
                         );
                     })}
-                </div>
-            </div>
+                </Col>
+            </Row>
         </>
     );
 }
