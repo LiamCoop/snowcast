a52da91288174306fe0799d5ff5b017125390164
Liam Cooper <liamcoop1997@gmail.com>
2021-03-21 00:11:08 -0700

    added interface, updated props for daybutton

--- src/components/DayButton/index.tsx
+++ src/components/DayButton/index.tsx
@@ -18,28 +18,30 @@ function GetIcon(array: Array<ConditionsObj>): string {
   return icon;
 }
 
-const DayButton = (props: {
+interface DayButtonProps {
   day: Array<ConditionsObj>;
   onClick: () => void;
   active: boolean;
   units: string;
-}) => {
+}
+
+const DayButton = ({ day, onClick, active, units }: DayButtonProps) => {
   let temp = 0;
   let daySnow = 0;
 
-  props.day.map((time) => {
+  day.map((time) => {
     daySnow += time.snow ? time.snow?.['3h'] : 0;
     temp += time.main.temp;
   });
   temp = Math.round(temp / 8);
 
   return (
-    <Button style={{ opacity: props.active ? 0.3 : 1 }} onClick={props.onClick}>
-      <TxtMd>{`${props.day[0].dt_txt.split(' ')[0]}`}</TxtMd>
+    <Button style={{ opacity: active ? 0.3 : 1 }} onClick={onClick}>
+      <TxtMd>{`${day[0].dt_txt.split(' ')[0]}`}</TxtMd>
       <TxtSm>
-        {temp} &deg;{props.units === 'metric' ? 'C' : 'F'}
+        {temp} &deg;{units === 'metric' ? 'C' : 'F'}
       </TxtSm>
-      <Icon icID={GetIcon(props.day)} imgHeight={'50vh'} />
+      <Icon icID={GetIcon(day)} imgHeight={'50vh'} />
       <TxtSm>Snow: {Math.round(daySnow * 100) / 10}mm</TxtSm>
     </Button>
   );
